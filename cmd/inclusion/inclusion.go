36916a7f45771730ccc5e24a032998114d0a2035
Jedrzej Nowak <pigmej@gmail.com>
2023-12-11 11:26:46 +0100

    Added batching to inclusion

--- cmd/inclusion/inclusion.go
+++ cmd/inclusion/inclusion.go
@@ -12,20 +12,18 @@ import (
 )
 
 var (
-	db   = flag.String("db", "", "database path")
-	from = flag.Int("from", 0, "from layer")
-	to   = flag.Int("to", 0, "to layer")
+	db      = flag.String("db", "", "database path")
+	from    = flag.Int("from", 0, "from layer")
+	to      = flag.Int("to", 0, "to layer")
+	batches = flag.Int("batches", 1, "number of batches")
 )
 
-func main() {
-	flag.Parse()
-	db, err := sql.Open("file:" + *db)
-	must(err)
+func calculateInclusionRates(db *sql.Database, fromLayer, toLayer int) {
 	var (
 		included float64
 		total    float64
 	)
-	for i := *from; i <= *to; i++ {
+	for i := fromLayer; i <= toLayer; i++ {
 		id, err := layers.GetApplied(db, types.LayerID(i))
 		must(err)
 		if id != types.EmptyBlockID {
@@ -43,6 +41,26 @@ func main() {
 		}
 	}
 	fmt.Printf("from = %d to = %d average inclusion %f\n", *from, *to, included/total)
+}
+
+func main() {
+	flag.Parse()
+	db, err := sql.Open("file:" + *db)
+	must(err)
+
+	if *from <= 0 {
+		*from = *from + *to
+	}
+
+	batchSize := (*to - *from + 1) / *batches
+	for batch := 0; batch < *batches; batch++ {
+		startLayer := *from + batch*batchSize
+		endLayer := startLayer + batchSize - 1
+		if batch == *batches-1 {
+			endLayer = *to
+		}
+		calculateInclusionRates(db, startLayer, endLayer)
+	}
 }
 
 func must(err error) {
