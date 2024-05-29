8300bcee10f7cdd8e98e242c775d974ffae450b2
Jedrzej Nowak <pigmej@gmail.com>
2024-05-29 14:28:00 +0200

    golangify

--- cmd/inclusion/inclusion.go
+++ cmd/inclusion/inclusion.go
@@ -4,6 +4,8 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/sql"
@@ -13,11 +15,10 @@ import (
 	"github.com/spacemeshos/go-spacemesh/sql/layers"
 
 	"bufio"
-	"os"
 )
 
 var (
-	db      = flag.String("db", "", "database path")
+	dbPath  = flag.String("db", "", "database path")
 	from    = flag.Int("from", 0, "from layer")
 	to      = flag.Int("to", 0, "to layer")
 	batches = flag.Int("batches", 1, "number of batches")
@@ -35,9 +36,11 @@ type Result struct {
 	totalNotCoinbases  uint64
 }
 
-func loadATXListFromFile(fpath string) []types.Address {
+func loadATXListFromFile(fpath string) ([]types.Address, error) {
 	file, err := os.Open(fpath)
-	must(err)
+	if err != nil {
+		return nil, err
+	}
 	defer file.Close()
 
 	var coinbaseList []types.Address
@@ -48,101 +51,127 @@ func loadATXListFromFile(fpath string) []types.Address {
 		var address types.Address
 		if err != nil {
 			address, err = types.StringToAddress(coinbase)
-			must(err)
+			if err != nil {
+				return nil, err
+			}
 		} else {
 			address = types.Address(notHex)
 		}
 		coinbaseList = append(coinbaseList, address)
 	}
-	must(scanner.Err())
-	return coinbaseList
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return coinbaseList, nil
 }
 
-func calculateInclusionRates(db *sql.Database, fromLayer, toLayer int) Result {
+func calculateInclusionRates(db *sql.Database, fromLayer, toLayer int) (Result, error) {
 	var (
-		included               float64
-		included_coinbases     float64
-		included_not_coinbases float64
-		total                  float64
-		total_coinbases        float64
-		total_not_coinbases    float64
+		included             float64
+		includedCoinbases    float64
+		includedNotCoinbases float64
+		total                float64
+		totalCoinbases       float64
+		totalNotCoinbases    float64
 	)
-	coinbaseList := loadATXListFromFile("coinbase_list.txt")
+	coinbaseList, err := loadATXListFromFile("coinbase_list.txt")
+	if err != nil {
+		return Result{}, err
+	}
 	coinbaseSet := make(map[string]struct{})
 	for _, coinbase := range coinbaseList {
 		coinbaseSet[coinbase.String()] = struct{}{}
 	}
 	for i := fromLayer; i <= toLayer; i++ {
 		id, err := layers.GetApplied(db, types.LayerID(i))
-		must(err)
+		if err != nil {
+			return Result{}, err
+		}
 		if id != types.EmptyBlockID {
 			block, err := blocks.Get(db, id)
-			must(err)
+			if err != nil {
+				return Result{}, err
+			}
 			included += float64(len(block.Rewards))
 			for _, reward := range block.Rewards {
 				atx, err := atxs.Get(db, reward.AtxID)
-				must(err)
+				if err != nil {
+					return Result{}, err
+				}
 				coinbase := atx.Coinbase.String()
 				if _, ok := coinbaseSet[coinbase]; ok {
-					included_coinbases += 1
+					includedCoinbases++
 				} else {
-					included_not_coinbases += 1
+					includedNotCoinbases++
 				}
 			}
 		}
 
 		ballots, err := ballots.Layer(db, types.LayerID(i))
-		must(err)
+		if err != nil {
+			return Result{}, err
+		}
 		for _, ballot := range ballots {
 			if ballot.IsMalicious() {
 				continue
 			}
-			total += 1
+			total++
 			atx, err := atxs.Get(db, ballot.AtxID)
-			must(err)
+			if err != nil {
+				return Result{}, err
+			}
 			coinbase := atx.Coinbase.String()
 			if _, ok := coinbaseSet[coinbase]; ok {
-				total_coinbases += 1
+				totalCoinbases++
 			} else {
-				total_not_coinbases += 1
+				totalNotCoinbases++
 			}
 		}
 	}
-	fmt.Printf("from = %d to = %d average inclusion %f\n    from list %f (%d)\n    not in list %f (%d)\n", fromLayer, toLayer, included/total, included_coinbases/total_coinbases, uint64(total_coinbases), included_not_coinbases/total_not_coinbases, uint64(total_not_coinbases))
+	fmt.Printf("from = %d to = %d average inclusion %f\n    from list %f (%d)\n    not in list %f (%d)\n", fromLayer, toLayer, included/total, includedCoinbases/totalCoinbases, uint64(totalCoinbases), includedNotCoinbases/totalNotCoinbases, uint64(totalNotCoinbases))
 	return Result{
 		fromLayer:          fromLayer,
 		toLayer:            toLayer,
 		averageInclusion:   included / total,
-		inclusionFromList:  included_coinbases / total_coinbases,
-		totalCoinbases:     uint64(total_coinbases),
-		inclusionNotInList: included_not_coinbases / total_not_coinbases,
-		totalNotCoinbases:  uint64(total_not_coinbases),
-	}
+		inclusionFromList:  includedCoinbases / totalCoinbases,
+		totalCoinbases:     uint64(totalCoinbases),
+		inclusionNotInList: includedNotCoinbases / totalNotCoinbases,
+		totalNotCoinbases:  uint64(totalNotCoinbases),
+	}, nil
 }
 
-func writeCsv(result Result) {
+func writeCsv(result Result) error {
 	file, err := os.OpenFile(*toFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	must(err)
+	if err != nil {
+		return err
+	}
 	defer file.Close()
 	writer := bufio.NewWriter(file)
 	defer writer.Flush()
 	_, err = writer.WriteString(fmt.Sprintf("%d,%d,%f,%f,%d,%f,%d\n", result.fromLayer, result.toLayer, result.averageInclusion, result.inclusionFromList, result.totalCoinbases, result.inclusionNotInList, result.totalNotCoinbases))
-	must(err)
+	return err
 }
 
 func main() {
 	flag.Parse()
-	db, err := sql.Open("file:" + *db)
-	must(err)
+	db, err := sql.Open("file:" + *dbPath)
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
 
 	if *from <= 0 {
-		*from = *from + *to
+		*from += *to
 	}
 
 	if *every > 0 {
 		for i := *from; i <= *to; i += *every {
-			result := calculateInclusionRates(db, i, i+*every-1)
-			writeCsv(result)
+			result, err := calculateInclusionRates(db, i, i+*every-1)
+			if err != nil {
+				log.Fatalf("failed to calculate inclusion rates: %v", err)
+			}
+			if err := writeCsv(result); err != nil {
+				log.Fatalf("failed to write CSV: %v", err)
+			}
 		}
 	} else {
 		batchSize := (*to - *from + 1) / *batches
@@ -152,15 +181,13 @@ func main() {
 			if batch == *batches-1 {
 				endLayer = *to
 			}
-			result := calculateInclusionRates(db, startLayer, endLayer)
-			writeCsv(result)
+			result, err := calculateInclusionRates(db, startLayer, endLayer)
+			if err != nil {
+				log.Fatalf("failed to calculate inclusion rates: %v", err)
+			}
+			if err := writeCsv(result); err != nil {
+				log.Fatalf("failed to write CSV: %v", err)
+			}
 		}
-	}
-
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
 	}
 }
